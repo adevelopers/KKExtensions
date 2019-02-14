9138165e1c6d7776bda403cb4663c232358b1fb7
Kirill Khudyakov <hudiakov@gmail.com>
2019-02-14 14:31:15 +0300

    Added first extension

---
+++ Extensions/FileManager+extensions.swift
@@ -0,0 +1,22 @@
+//
+//  FileManager+extensions.swift
+//  AudioReminder
+//
+//  Created by Kirill Khudiakov on 29/01/2019.
+//  Copyright © 2019 adeveloper. All rights reserved.
+//
+
+import Foundation
+
+extension FileManager {
+    
+    class func getDocumentDirectory() -> URL? {
+        guard let documentURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            print("Faild to get the document directory")
+            return nil
+        }
+        
+        return documentURL
+    }
+    
+}
